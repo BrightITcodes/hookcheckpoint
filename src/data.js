5d29c4d7889e347b84ef75d066685233b8e5187f
unknown <arobani.sunday2019@nda.edu.ng>
2023-11-26 21:32:06 +0100

    adding hook

---
+++ src/data.js
@@ -0,0 +1,67 @@
+const movies = [
+  {
+    id: 1,
+    title: "Gravity",
+    description: "A breathtaking space thriller directed by Alfonso Cuarón.",
+    postUrl:
+      "https://upload.wikimedia.org/wikipedia/en/f/f6/Gravity_Poster.jpg?20170222003810",
+    rating: 1,
+  },
+  {
+    id: 2,
+    title: "The Martian",
+    description:
+      "Ridley Scott's compelling story of an astronaut's struggle for survival on Mars.",
+    postUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQi6gkd7gGtGW6bri92wunYgtLuwaL5-nIuik-i2AC5gLiB3RIX4KPSvfa_DcC8w-fzkFE&usqp=CAU",
+    rating: 2,
+  },
+  {
+    id: 3,
+    title: "Whiplash",
+    description:
+      "An intense drama about the pursuit of greatness in the world of jazz music.",
+    postUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRp1HbvtMHXtq4WiVosmyBje60OIFkZeOb4uQ&usqp=CAU",
+    rating: 3,
+  },
+  {
+    id: 4,
+    title: "Coco",
+    description:
+      "A heartwarming animated film celebrating family, culture, and music.",
+    postUrl:
+      "https://th.bing.com/th?id=OIP.CGk6h0oYsE5h6Yme72Wh1gHaK-&w=205&h=304&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2",
+    rating: 4,
+  },
+  {
+    id: 5,
+    title: "The Prestige",
+    description:
+      "Christopher Nolan's mesmerizing tale of rival magicians in Victorian-era London.",
+    postUrl:
+      "https://th.bing.com/th/id/OIP.5mSOkCAI8cczOKAV6QIGVAHaEK?w=251&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+    rating: 5,
+  },
+  {
+    id: 6,
+    title: "Amélie",
+    description:
+      "A whimsical and charming French film about an imaginative young woman in Paris.",
+    postUrl:
+      "https://th.bing.com/th/id/OIP.QfWvWpE64XwF_SvCFKqD6gHaLH?w=115&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+    rating: 6,
+  },
+  {
+    id: 7,
+    title: "The Imitation Game",
+    description:
+      "A gripping biographical drama about Alan Turing and the Enigma code.",
+    postUrl:
+      "https://th.bing.com/th?id=OIP.GGJ8-dW4HLPzBhgGQdXaAwHaLH&w=204&h=306&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2",
+    rating: 7,
+  },
+];
+ 
+export default movies;
+
